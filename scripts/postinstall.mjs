59b3364d28424af8264f4f819248f902d51468cc
Kiko Beats <josefrancisco.verdu@gmail.com>
2025-12-30 09:03:02 +0100

    chore(postinstall): handle res.ok

--- scripts/postinstall.mjs
+++ scripts/postinstall.mjs
@@ -1,14 +1,19 @@
 import { writeFile } from 'fs/promises'
 
-const mapper = rates =>
-  Object.keys(rates).reduce(
-    (acc, alpha2) => ({ ...acc, [alpha2]: rates[alpha2].rate })
-  )
-
-fetch(
+const URL =
   'https://raw.githack.com/valeriansaliou/node-sales-tax/master/res/sales_tax_rates.json'
-)
-  .then(res => res.json())
+
+const mapper = rates =>
+  Object.keys(rates).reduce((acc, alpha2) => ({
+    ...acc,
+    [alpha2]: rates[alpha2].rate
+  }))
+
+fetch(URL)
+  .then(res => {
+    if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`)
+    return res.json()
+  })
   .then(rates =>
     writeFile('src/rates.json', JSON.stringify(mapper(rates), null, 2))
   )
